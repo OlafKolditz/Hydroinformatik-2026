429c1b10dfc9ed929f8759de0999542061bd3fcd
KolditzTUDD <olaf.kolditz@tu-dresden>
2026-05-07 19:27:07 +0200

    Upload of exercises

---
+++ EX01-main-function.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  std::cout << "Hallo C++ World, heute ist der 08.05.2026, der Tag fühlt sich seltsam an ..." << std::endl;
+  return 0;
+}
+
---
+++ EX02a-data-types-bool.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "E02a-data-types: bool\n";
+  bool x = 17 > 23;
+  cout << "Ist 17 grösser als 23? " << x << std::endl;
+  return 0;
+}
+
